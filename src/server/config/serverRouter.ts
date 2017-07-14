d5aed136a388d594cdb10e32d6905b5c0ea11bf5
laurts <lyys.lauri@gmail.com>
2017-07-14 10:00:15 +0300

    fixed server router

--- src/server/config/serverRouter.ts
+++ src/server/config/serverRouter.ts
@@ -1,24 +1,26 @@
 import * as Router from "universal-router";
 export function serverRouter(routes) {
-  return new Router(routes, resolveRoute);
-}
 
-
-function resolveRoute(context, params) {
-  if (typeof context.route.action === 'function') {
-    let props = {};
-    if (typeof context.route.getInitialProps === 'function') {
-      return Promise.resolve(context.route.getInitialProps(context))
-        .then(props => {
-          context.props = props;
-          if (props) {
-            context.store[context.route.path] = props;
-          }
-          return context.route.action(context, params)
-        });
+  function resolveServerRoute(context, params) {
+    if (typeof context.route.action === 'function') {
+      let props = {};
+      if (typeof context.route.getInitialProps === 'function') {
+        return Promise.resolve(context.route.getInitialProps(context))
+          .then(props => {
+            context.props = props;
+            if (props) {
+              context.store[context.route.path] = props;
+            }
+            return context.route.action(context, params)
+          });
+      }
+      return context.route.action(context, params);
     }
-    return context.route.action(context, params);
+
+    return null;
   }
 
-  return null;
+  return new Router(routes, { resolveRoute:resolveServerRoute });
 }
+
+
