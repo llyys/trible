710d3060167c30674425ef23c2437710fd58afde
laurts <lyys.lauri@gmail.com>
2017-07-30 11:46:02 +0300

    Added better logging to make internals more transparent

--- src/server/config/alias.ts
+++ src/server/config/alias.ts
@@ -1,18 +1,25 @@
-import * as path from 'path'
+import * as path from "path";
+import logger from "../../lib/Logger";
+const log = logger.get(module);
+const chalk: any = require("chalk");
 
-var module_require:any = require('module')
+var module_require: any = require("module");
 var Module = module_require.Module;
 
 var oldLoad = Module._load;
-Module._load = function (request, parent, isMain) {
-  var extension = path.extname(request) || '.js';
-  if (extension === '.svg') {
-    console.log(`ignoring file in server render ${request}`)
-    return {}
+Module._load = function(request, parent, isMain) {
+  var extension = path.extname(request) || ".js";
+  if (extension === ".svg") {
+    log.debug(`ignoring file in server render ${request}`);
+    return {};
   }
-  if (extension === '.scss') {
-    let cssModel = path.resolve(path.dirname(parent.filename), request + '.json');
-    console.log(`ignoring file in server render ${request}`)
+  if (extension === ".scss") {
+    // When using css modules we have generated json file to map the classnames with generated css names...
+    let cssModel = path.resolve(
+      path.dirname(parent.filename),
+      request + ".json"
+    );
+    log.debug(`mapping ${request} with ${chalk.bold.white(cssModel)}`);
     let obj = require(cssModel);
     return obj;
   }
@@ -20,5 +27,7 @@ Module._load = function (request, parent, isMain) {
   return res;
 };
 
-const moduleAlias:any = require('module-alias');
-moduleAlias.addAlias('~', path.join(__dirname, '../../'));
+//Since typescript compiler won't replace the ~ mapings so this code will do this for the runtime
+const moduleAlias: any = require("module-alias");
+log.debug(`mapping ~/ to ${path.join(__dirname, "../../")}`);
+moduleAlias.addAlias("~", path.join(__dirname, "../../")); //map ~ to the correct path.
--- src/server/config/serverRouter.ts
+++ src/server/config/serverRouter.ts
@@ -1,19 +1,19 @@
 import * as Router from "universal-router";
 //Resolve Universal react routing components in server side
 export function serverRouter(routes) {
-
   function resolveServerRoute(context, params) {
-    if (typeof context.route.action === 'function') {
+    if (typeof context.route.action === "function") {
       let props = {};
-      if (typeof context.route.getInitialProps === 'function') {
-        return Promise.resolve(context.route.getInitialProps(context))
-          .then(props => {
-            context.props = props;
-            if (props) {
-              context.store[context.route.path] = props;
-            }
-            return context.route.action(context, params)
-          });
+      if (typeof context.route.getInitialProps === "function") {
+        return Promise.resolve(
+          context.route.getInitialProps(context)
+        ).then(props => {
+          context.props = props;
+          if (props) {
+            context.store[context.route.path] = props;
+          }
+          return context.route.action(context, params);
+        });
       }
       return context.route.action(context, params);
     }
@@ -21,9 +21,5 @@ export function serverRouter(routes) {
     return null;
   }
 
-  return new Router(routes, { resolveRoute:resolveServerRoute });
+  return new Router(routes, { resolveRoute: resolveServerRoute });
 }
-
-
-
-
